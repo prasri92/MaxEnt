Fixed KL divergence

src/codebase/data_helper.py
@@ -256,6 +256,7 @@ class DataHelper:
         - A list containing probabilities for all possible 2^N disease vectors.
         """
         probs = []
+        marginal_probs = np.zeros(self.N+1)
         total=0.0
         if timer:
             tic = time.time()
@@ -263,14 +264,15 @@ class DataHelper:
             b = format(idx, '0{}b'.format(self.N))
             r = [int(j) for j in b]
             p = self.computeProbability(r, overlap=overlap)
-            
+            m = sum(r)
+            marginal_probs[m]+=p
             probs.append(p)
             total+=probs[-1]
         print('Sum of probabilities = {}'.format(total))
         if timer:
             toc = time.time()
             print('Computational time for {} probabilities = {} seconds'.format(2**self.N, toc-tic))
-        return probs
+        return probs,marginal_probs
 '''
 #non-overlapping case 
 def run(outfilename, d, c, e, tau, beta, p):
@@ -290,9 +292,9 @@ def run(outfilename, d, c, e, tau, beta, p, q1, q2):
         alpha.append(stats.expon.pdf(i, scale=e))
     alpha = np.array(alpha)/sum(alpha)
     data = DataHelper(d, c, alpha, tau, beta, p, q1, q2)
-    p_vals = data.computeAll(timer=True, overlap=True)
+    p_vals, marginals = data.computeAll(timer=True, overlap=True)
     with open(outfilename, "wb") as outfile:
-        pickle.dump(p_vals, outfile)
+        pickle.dump((p_vals, marginals), outfile)
 
 if __name__=='__main__': 
     # example case

src/codebase/optimizer.py
@@ -339,13 +339,13 @@ class Optimizer(object):
         findpos = {elem:i for i,elem in enumerate(partition)}       
 
         # Create all permuatations of a vector belonging to that partition
-        all_perms = list(itertools.product([0, 1], repeat=num_feats))[1:]
-        num_total_vectors = 2**(num_feats)-1
+        # all_perms = list(itertools.product([0, 1], repeat=num_feats))[1:]
+        # num_total_vectors = 2**(num_feats)-1
         # print('length of all_perms', len(all_perms)==num_total_vectors)
 
 
-        # all_perms = itertools.product([0, 1], repeat=num_feats)
-        # num_total_vectors = 2**(num_feats)
+        all_perms = itertools.product([0, 1], repeat=num_feats)
+        num_total_vectors = 2**(num_feats)
         constraint_mat = np.zeros((num_total_vectors, len_theta))        
                 
 
@@ -380,8 +380,8 @@ class Optimizer(object):
             norm_sum = 1 + np.exp(thetas[0])
             return np.log(norm_sum)            
         
-        num_total_vectors = 2**(num_feats)-1
-        # num_total_vectors = 2**(num_feats)
+        # num_total_vectors = 2**(num_feats)-1
+        num_total_vectors = 2**(num_feats)
         inner_array = np.dot(constraint_mat, thetas)
         
         log_norm = 0.0
@@ -410,8 +410,8 @@ class Optimizer(object):
             return norm_sum
 
         # Create all permuatations of a vector belonging to that partition
-        # all_perms = itertools.product([0, 1], repeat=num_feats)
-        all_perms = list(itertools.product([0, 1], repeat=num_feats))[1:]
+        all_perms = itertools.product([0, 1], repeat=num_feats)
+        # all_perms = list(itertools.product([0, 1], repeat=num_feats))[1:]
 
         for vec in all_perms:
             tmpvec = np.asarray(vec)

src/codebase/synthetic_data.py
@@ -5,14 +5,14 @@ import numpy as np
 import sys
 
 #Define global parameters 
-num_diseases = 2
-clusters = 2
+num_diseases = 20
+clusters = 4
 
 expon_parameter = [0.8, 1.6, 2.4, 3.2, 4.0]
 zipfian_parameter = [0.0, 1.0, 2.0, 3.0, 4.0]
 
 #Change for each expt 
-size = 25
+size = 500
 
 def generate_tau(z):
 	x = np.arange(1, clusters+1)
@@ -20,11 +20,11 @@ def generate_tau(z):
 	tau = tau/sum(tau)
 	return tau
 
-#check what kind of distribution it should be - as of now uniform
-beta = [0.5, 0.5]
-p = 0.5
-q1 = 0.5
-q2 = 0.5
+#check what kind of distribution it should be? 
+beta = [0.3, 0.2, 0.4, 0.1]
+p = 0.6
+q1 = 0.3
+q2 = 0.7
 
 def get_true_distribution(file_name_real, tau, l):
 	print(data_helper.run(file_name_real, num_diseases, clusters, l, tau, beta, p, q1, q2))
@@ -54,8 +54,8 @@ def main():
 '''
 def main(l, z, file_num):
 	tau = generate_tau(z)
-	file_name_real = '../../output/'+'d'+str(size)+'_2/truedist_expt'+str(file_num)+'.pickle'
-	file_name_synthetic = "../../dataset/"+"d"+str(size)+"_2/synthetic_data_expt"+str(file_num)+".csv"
+	file_name_real = '../../output/'+'d'+str(size)+'_20/truedist_expt'+str(file_num)+'.pickle'
+	file_name_synthetic = "../../dataset/"+"d"+str(size)+"_20/synthetic_data_expt"+str(file_num)+".csv"
 	p1 = Process(target=get_true_distribution, args=(file_name_real, tau, l))
 	p2 = Process(target=get_synthetic_data, args=(file_name_synthetic, l, z, beta, size))
 	p1.start()

src/kl_div_plot.py
@@ -18,38 +18,42 @@ def read_prob_sum(filename):
 		prob = pickle.load(outfile,encoding='latin1')
 	return prob[1], prob[2]
 
+def read_true_prob(filename):
+	with open(filename, "rb") as outfile:
+		prob = pickle.load(outfile,encoding='latin1')
+	return prob[1]
+
 def kl_divergence(p, q):
 	return (p*np.log(p/q)).sum()
 
 
-def main_1(i, j):
+def compute_prob(i, j=None):
 	actual = '../output/d50_4/truedist_expt'+str(i)+'.pickle'
-	# synthetic = '../output/d50_4_manual/syn_maxent_expt'+str(i)+'.pickle' #'_support_0.4.pickle'
-	synthetic = '../output/d50_4_manual/syn_maxent_expt'+str(i)+'_constraints_'+str(j)+'.pickle' 
+	synthetic = '../output/d50_4/syn_maxent_expt'+str(i)+'_support_0.01.pickle'
 	p = (np.array(read_prob_dist(actual)))
 	q = (np.array(read_prob_dist(synthetic)))
 	maxent_prob, emp_prob = read_prob_sum(synthetic)
 	kl_div = kl_divergence(q,p)
-
-	return maxent_prob, emp_prob, kl_div
+	true_prob = read_true_prob(actual)
+	return maxent_prob, emp_prob, kl_div, true_prob
 
 if __name__ == '__main__':
 	maxent_prob = []
 	emp_prob = []
 	kl = []
-	# for i in range(1,26):
+	true_prob = []
+
 	for i in range(1, 26):
-		m, e, k = main_1(i, 7)
+		m, e, k, t = compute_prob(i)
 		maxent_prob.append(m)
 		emp_prob.append(e)
 		kl.append(k)
+		true_prob.append(t)
 
-	# num_feats = 20
 	num_feats = 4
 	xvec = [i for i in range(num_feats+1)]
-	x_ticks = np.arange(-1, num_feats+1, 1.0)
+	x_ticks = np.arange(0, num_feats+1, 1.0)
 	plot_lims = [-1,  num_feats+1, -0.1, 1.0]
-
 
 	'''
 	fig, (ax0,ax1,ax2,ax3,ax4) = plt.subplots(1,5, figsize=(30,10))
@@ -82,14 +86,14 @@ if __name__ == '__main__':
 	# ax15, ax16, ax17, ax18, ax19, ax20, ax21, ax22, ax23, ax24, ax25, ax26, ax27, ax28, ax29]
 
 	# fig, ((ax0,ax1,ax2,ax3,ax4), (ax5, ax6, ax7, ax8, ax9), \
-	# 	(ax10,ax11, ax12, ax13, ax14)) = plt.subplots(3,5, figsize=(100, 50))
+	# 	(ax10,ax11, ax12, ax13, ax14)) = plt.subplots(3,5, figsize=(40, 20))
 	# lst = [ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10, ax11, ax12, ax13, ax14]
 
 	data = ["File Number"]
 	for i in range(5):
 		data.append(i)
 
-	out = csv.writer(open("../output/prob_dist/nz_4d_s50_manual_fourway.csv","w"), delimiter=',',quoting=csv.QUOTE_ALL)
+	out = csv.writer(open("../output/prob_dist/4d_size50_support0.01.csv","w"), delimiter=',',quoting=csv.QUOTE_ALL)
 	out.writerow(data)
 	
 	print(len(maxent_prob))
@@ -101,6 +105,7 @@ if __name__ == '__main__':
 		
 		i.plot(xvec, maxent_prob[num], 'r', label='Maxent')
 		i.plot(xvec, emp_prob[num], 'b', label='Empirical')
+		i.plot(xvec, true_prob[num], 'g', label='True')
 		i.axis(plot_lims)
 		i.set_xticks(x_ticks)
 		# for xy in zip(xvec, maxent_prob[num]):
@@ -115,9 +120,9 @@ if __name__ == '__main__':
 			i.set_title('KL div = ' + str(kl[num]) + '\nLambda = 0.3125, Skew = ' + str(num%5), fontsize=7)
 		elif num//5 == 4:
 			i.set_title('KL div = ' + str(kl[num]) + '\nLambda = 0.25, Skew = '+ str(num%5), fontsize=7)
-		i.legend()
+		i.legend(fontsize=6)
 		row = []
 
-	fig.suptitle('Diseases = 4\nDataset Size = 50', fontsize=10)
+	fig.suptitle('Diseases = 4\nDataset Size = 50\nSupport=0.01', fontsize=10)
 	plt.subplots_adjust(hspace = 0.6)
 	plt.show()

src/main_kl_test.py
@@ -19,16 +19,15 @@ from codebase.utils import load_disease_data
 from codebase.extract_features import ExtractFeatures
 from codebase.optimizer import Optimizer
 
-'''
-Support v4 
-l = 0.8, support = 0.002
-l = 1.6, support = 0.005
-l = 2.4, support = 0.005 (to get 2,3,4 way constraints)
-l = 3.2, support = 0.022
-l = 4.0, support = 0.029
-'''
-
 def marketbasket(cleaneddata, support):
+    '''
+    Function to use market basket analysis to return 2 way, 3 way and 4 way constraints. 
+    Input: 
+        Data vectors from synthetically generated data 
+        Support: Support for the apriori algorithm
+    Returns: 
+        Dictionary containing 2 way, 3 way and 4 way constraints 
+    '''
     frequent_itemsets = apriori(cleaneddata, min_support=support, use_colnames=True)
     rules = association_rules(frequent_itemsets, metric="lift", min_threshold=0.002)
     rules["antecedent_len"] = rules["antecedents"].apply(lambda x: len(x))
@@ -84,9 +83,7 @@ def marketbasket(cleaneddata, support):
         for j in i:
             tmp.append(int(float(j)))
         lfour.append(tmp)
-    # print(sets_two, sets_three, sets_four)
-    print(ltwo, lthree, lfour)
-
+    
     """Output two way, three way and four way dictionaries as input for optimization"""
     for stwo in ltwo:
         two_way_dict[tuple(stwo)]=val_two
@@ -94,22 +91,29 @@ def marketbasket(cleaneddata, support):
         three_way_dict[tuple(sthree)]=val_three
     for sfour in lfour:
         four_way_dict[tuple(sfour)]=val_four
+    
     return two_way_dict, three_way_dict, four_way_dict
+
+def read_prob_dist(filename):
+    with open(filename, "rb") as outfile:
+        prob = pickle.load(outfile,encoding='latin1')
+    return prob[1]
 
 def compute_prob_exact(optobj):
     maxent_prob = []
     num_feats = optobj.feats_obj.data_arr.shape[1]
-    maxent_diseases = np.zeros(num_feats+1)
+    maxent_sum_diseases = np.zeros(num_feats+1)
     all_perms = itertools.product([0, 1], repeat=num_feats)
     total_prob = 0.0    # finally should be very close to 1
     for tmp in all_perms:
         vec = np.asarray(tmp)
         p_vec = optobj.prob_dist(vec)
-        print(p_vec)
+        print('Vector:', vec, ' Probability: ', p_vec)
         j = sum(vec)
-        maxent_diseases[j] += p_vec
+        maxent_sum_diseases[j] += p_vec
         total_prob += p_vec
         maxent_prob.append(p_vec) 
+    
     print('Total Probability: ', total_prob)
 
     emp_prob = np.zeros(num_feats + 1)
@@ -118,9 +122,7 @@ def compute_prob_exact(optobj):
         emp_prob[j] += 1
     emp_prob /= optobj.feats_obj.data_arr.shape[0]
     
-    print("Empirical probabilities: " +str(emp_prob))
-    print("maxent_diseases: " + str(maxent_diseases))
-    return maxent_prob, maxent_diseases, emp_prob
+    return maxent_prob, maxent_sum_diseases, emp_prob
 
 def main(file_num=None):
     print("File num: " + str(file_num) + " has started")
@@ -135,13 +137,13 @@ def main(file_num=None):
     #     14:0.015, 15:0.012, 16:0.02, 17:0.022, 18:0.018, 19:0.022, 20:0.019, 21:0.028, \
     #     22:0.032, 23:0.03, 24:0.029, 25:0.1} #25:0.035}
     # support = support_data_overlap[file_num]
-    support = 0.8
+    support = 0.3
     
     tic = time.time()
     # real data
     # directory = '../dataset/basket_sets.csv'
     # generating synthetic data 
-    directory = '../dataset/d25_2/synthetic_data_expt'+str(file_num)+'.csv'
+    directory = '../dataset/d50_4/synthetic_data_expt'+str(file_num)+'.csv'
     cleaneddata=pd.read_csv(directory, error_bad_lines=False)
     
     two_wayc, three_wayc, four_wayc = marketbasket(cleaneddata, support)
@@ -150,6 +152,7 @@ def main(file_num=None):
     # four_wayc = {}
 
     data_array = load_disease_data(directory)
+    # data_array = load_disease_data(cleaneddata.values)
     feats = ExtractFeatures(data_array)
 
     feats.set_two_way_constraints(two_wayc)
@@ -168,12 +171,16 @@ def main(file_num=None):
     print("Optimizer is done. Computing probabilities")
 
     maxent, sum_prob_maxent, emp_prob = compute_prob_exact(opt)
+    print("Empirical: " +str(emp_prob))
+    print("Maxent: " + str(sum_prob_maxent))
+    print("True distribution:" + str(read_prob_dist('../output/d50_4/truedist_expt'+str(file_num)+'.pickle')))
+    
     print("writing to file")
 
     # for real data
     # outfilename = '../output/realdata_maxent.pickle'
     # for synthetic data 
-    outfilename = '../output/d25_2/syn_maxent_expt'+str(file_num)+'_support_'+str(support)+'.pickle'
+    outfilename = '../output/d50_4/syn_maxent_expt'+str(file_num)+'_support_'+str(support)+'.pickle'
 
     with open(outfilename, "wb") as outfile:
         pickle.dump((maxent, sum_prob_maxent, emp_prob), outfile)